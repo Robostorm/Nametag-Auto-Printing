2cdece1a61db2f6f77d7d5f447541577b672c434
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-06-11 15:59:09 -0400

    Added printers to settings menu

---
+++ src/nametagautoprint/Printer.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nametagautoprint;
+
+import java.io.File;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author tim
+ */
+public class Printer {
+    
+    private String name;
+    private String ip;
+    private int port;
+    private File config;
+    private boolean active;
+    
+    private GridPane grid;
+    private Label nameLabel;
+    private TextField ipField;
+    private TextField portField;
+    private TextField configField;
+    private Button configButton;
+    private CheckBox activeBox;
+    
+    public Printer(String name){
+        this.name = name;
+        this.ip = "127.0.0.1";
+        this.port = 5000;
+        this.config = new File("config/slic3r/mendel.ini");
+        this.active = false;
+    }
+    
+    public Printer(String name, String ip, int port){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+        this.config = new File("config/slic3r/mendel.ini");
+        this.active = false;
+        
+        init();
+    }
+    
+    public Printer(String name, String ip, int port, String config){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+        this.config = new File(config);
+        this.active = false;
+        
+        init();
+    }
+    
+    public Printer(String name, String ip, int port, File config){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+        this.config = config;
+        this.active = false;
+        
+        init();
+    }
+    
+    public Printer(String name, String ip, int port, String config, boolean active){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+        this.config = new File(config);
+        this.active = active;
+        
+        init();
+    }
+    
+    public Printer(String name, String ip, int port, File config, boolean active){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+        this.config = config;
+        this.active = active;
+        
+        init();
+    }
+    
+    // So that we don't need to repeat this in every constructor
+    private void init(){
+        this.grid = new GridPane();
+        
+        this.grid.setHgap(10);
+        this.grid.setVgap(10);
+        this.grid.setPadding(new Insets(10, 10, 10, 10));
+        this.grid.setBackground(new Background(new BackgroundFill(Paint.valueOf("yellow"), CornerRadii.EMPTY, Insets.EMPTY)));
+        //this.grid.setGridLinesVisible(true);
+        
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(20);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(30);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(7);
+        ColumnConstraints column4 = new ColumnConstraints();
+        column4.setPercentWidth(100-(column1.getPercentWidth()+column2.getPercentWidth()+column3.getPercentWidth()));
+        column4.setHalignment(HPos.RIGHT);
+        this.grid.getColumnConstraints().addAll(column1, column2, column3, column4);
+        
+        this.nameLabel = new Label(name);
+        this.nameLabel.setFont(new Font("Sans-Serif", 30));
+        this.grid.add(nameLabel, 0, 0, 1, 3);
+        
+        this.ipField = new TextField(ip);
+        this.grid.add(ipField, 1, 0, 1, 1);
+        
+        this.portField = new TextField(Integer.toString(port));
+        this.grid.add(portField, 2, 0, 1, 1);
+        
+        this.configField = new TextField(config.getPath());
+        this.grid.add(configField, 1, 1, 1, 1);
+        
+        this.configButton = new Button("Choose");
+        this.grid.add(configButton, 2, 1, 1, 1);
+        
+        this.activeBox = new CheckBox();
+        this.activeBox.setSelected(active);
+        this.grid.add(activeBox, 3, 0, 1, 2);
+    }
+    
+    public Pane getPane(){
+        return (Pane) this.grid;
+    }
+    
+}
--- src/nametagautoprint/SettingsController.java
+++ src/nametagautoprint/SettingsController.java
@@ -8,7 +8,9 @@ package nametagautoprint;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -17,12 +19,16 @@ import javafx.fxml.Initializable;
  */
 public class SettingsController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML VBox printerList;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        Printer test = new Printer("Robostorm", "3247215", 1234);
+        Printer test2 = new Printer("Test2", "sdewrqewrvqewr ", 5678);
+        
+        printerList.getChildren().addAll(test.getPane(), test2.getPane());
+        
     }    
     
 }
